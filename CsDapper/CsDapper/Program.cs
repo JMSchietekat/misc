3832a76872aeb44244615035ddaf0e130af44d33
Justin Schietekat <justinschietekat@gmail.com>
2020-12-18 12:11:31 +0200

    Test drive dapper in c#.

---
+++ CsDapper/CsDapper/Program.cs
@@ -0,0 +1,34 @@
+﻿using Dapper;
+using System.Data.SqlClient;
+using System.Linq;
+using static System.Console;
+using static System.Environment;
+
+namespace CsDapper
+{
+    
+    public class Farm
+    {
+        public int IdFarm { get; }
+        public string Description { get; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Hello World!");
+
+            var connectionString = GetEnvironmentVariable("sqlConnectionString");
+            const string query = @"SELECT IdFarm, Description FROM dbo.Farm WHERE IdStatus < 4;";
+
+            using var connection = new SqlConnection(connectionString);
+            
+            var farms = connection.QueryAsync<Farm>(query).Result.ToList();
+
+            WriteLine(farms.Count);
+            
+            farms.ForEach(farm => WriteLine(farm.IdFarm + ": " + farm.Description));
+
+        }
+    }
+}
--- README.md
+++ README.md
@@ -7,3 +7,4 @@ I endeavour to maintain the below table as history and reference for myself and 
 | Date       | Lang/Framework | Source |
 | ---------- | -------------- | ------ |
 | 2020-12-17 | F#             | [Building a RESTful WebApi with F# and NetCore](https://nicolaiarocci.com/building-a-restful-webapi-with-fsharp-and-netcore/)
+| 2020-12-17 | C#  			  | [Dapper ORM](https://dapper-tutorial.net/)
